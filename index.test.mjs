228ab18f6e8819c56d4ef86a999f38e28a254bc6
Eli Sterling <eli@eliware.org>
2026-08-07 14:28:00 +0000

    Clean up lint warning

--- index.test.mjs
+++ index.test.mjs
@@ -1,4 +1,4 @@
-import { jest, test, expect } from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import fs from 'fs';
 import path from 'path';
 import { Resampler } from './index.mjs';
